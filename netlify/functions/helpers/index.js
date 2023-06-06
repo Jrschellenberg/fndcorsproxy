7e901eb800bcf43bea4063c0b2ba697076397ad4
Justin Schellenberg <jrschellenberg@gmail.com>
2023-06-06 16:24:04 -0500

    Logic for Storing Store Credit backend

--- netlify/functions/helpers/index.js
+++ netlify/functions/helpers/index.js
@@ -4,10 +4,13 @@ import * as crypto from 'node:crypto';
 const key = crypto.createHash('sha256').update(process.env.PRIVATE_KEY).digest();
 const iv = crypto.createHash('md5').update(process.env.PRIVATE_IV).digest();
 
+// Constant Salt is used for testing to not affect customer ID but update things.
+const CONSTANT_SALT = '3'
 
 export function encrypt(data) {
+  const string = JSON.stringify(data);
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-  let encryptedData = cipher.update(data, 'utf8', 'hex');
+  let encryptedData = cipher.update(string, 'utf8', 'hex');
   encryptedData += cipher.final('hex');
   return encryptedData;
 }
@@ -16,12 +19,12 @@ export function decrypt(encryptedData) {
   const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
   let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
   decryptedData += decipher.final('utf8');
-  return decryptedData;
+  return JSON.parse(decryptedData);
 }
 
 export function generateGiftCardCode(input) {
   const sha1 = crypto.createHash('sha1');
-  sha1.update(input.toString());
+  sha1.update(input.toString() + CONSTANT_SALT);
   const fullHash = sha1.digest('hex');
   const partialHash = fullHash.substring(0, 16);
   return `${partialHash}${process.env.NODE_ENV === 'development' ? 'TEST' : 'CRED' }`;
--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.post('/issue_store_credit', verifyRequest, bindShopifyService, async (req, res, next) => {
+router.post('/store_credit', verifyRequest, bindShopifyService, async (req, res, next) => {
   const { amount, customerId, name, email, phone } = req.body;
   if(!amount || !customerId || !name || !email || !phone ){
     const err = new Error('Require field of "shop", "amount", "customerId, name, email, and phone"');
@@ -14,7 +14,7 @@ router.post('/issue_store_credit', verifyRequest, bindShopifyService, async (req
   }
 
   try {
-    const response = await res.locals.shopify.post('/gift_cards.json', {
+    let response = await res.locals.shopify.post('/gift_cards.json', {
       "gift_card": {
         "note": `Store Credit for ${name}\nPhone:${phone}\nEmail:${email}`,
         "initial_value": amount.toString(),
@@ -23,14 +23,45 @@ router.post('/issue_store_credit', verifyRequest, bindShopifyService, async (req
       }
     });
 
+    console.log("did we get here?");
+
+    const encryptedGiftCardData = encrypt(response?.data?.gift_card);
+
+    console.log("EncryptedGiftCardData is ", encryptedGiftCardData)
+
+    await res.locals.shopify.post(`/customers/${customerId}/metafields.json`, {
+      "metafield": {
+        "namespace": "fnd",
+        "key": "encrypted_gift_card",
+        "type": "single_line_text_field",
+        "value": encryptedGiftCardData,
+      }
+    });
+
+    console.log("SECOND d2id we get here?")
+
+
+    await res.locals.shopify.post(`/customers/${customerId}/metafields.json`, {
+      "metafield": {
+        "namespace": "fnd",
+        "key": "store_credit",
+        "type": "number_decimal",
+        "value": "0.00"
+      }
+    });
+
+    console.log("THIRD did we get here?")
+
+    res.status(201).json({
+      message: `Successfully Issued Store Credit Stored as ${encryptedGiftCardData}`
+    });
   }
   catch(e) {
-    console.error(e);
+    e = e.toJSON();
+    const err = new Error(`Shopify Err: ${e.message}`)
+    err.status = e.status;
+    return next(err)
   }
-
-
-  console.log(response);
-  res.status(200).json({});
 });
 
 router.get('/', verifyRequest, async (req, res, next) => {
