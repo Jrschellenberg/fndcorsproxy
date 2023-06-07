350faa415734290bfb34faaf3337b59479246d34
Justin Schellenberg <jrschellenberg@gmail.com>
2023-06-07 15:47:52 -0500

    re move logs

--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -69,14 +69,9 @@ router.put('/store_credit', verifyRequest, bindShopifyService, async (req, res, 
   try {
     let response = await res.locals.shopify.get(`/gift_cards/${decryptedData.id}.json`);
 
-    console.log(response, response.data);
-
     const { id, balance, disabled_at } = response.data?.gift_card;
 
     let total = amount;
-
-    console.log(id, balance, disabled_at, !disabled_at, parseFloat(balance) > 0);
-
     if(!disabled_at && parseFloat(balance) > 0 ){
       total += parseFloat(balance);
       await res.locals.shopify.post(`/gift_cards/${id}/disable.json`);
