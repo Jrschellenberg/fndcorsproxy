1ed80e2d628bc65f4a1cfe865ae60c997081fdf8
Justin Schellenberg <jrschellenberg@gmail.com>
2023-06-06 23:05:13 -0500

    Update to unique hashcodes

--- netlify/functions/helpers/index.js
+++ netlify/functions/helpers/index.js
@@ -4,9 +4,6 @@ import * as crypto from 'node:crypto';
 const key = crypto.createHash('sha256').update(process.env.PRIVATE_KEY).digest();
 const iv = crypto.createHash('md5').update(process.env.PRIVATE_IV).digest();
 
-// Constant Salt is used for testing to not affect customer ID but update things.
-const CONSTANT_SALT = '3'
-
 export function encrypt(data) {
   const string = JSON.stringify(data);
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
@@ -24,7 +21,7 @@ export function decrypt(encryptedData) {
 
 export function generateGiftCardCode(input) {
   const sha1 = crypto.createHash('sha1');
-  sha1.update(input.toString() + CONSTANT_SALT);
+  sha1.update(input.toString() + Date.now());
   const fullHash = sha1.digest('hex');
   const partialHash = fullHash.substring(0, 16);
   return `${partialHash}${process.env.NODE_ENV === 'development' ? 'TEST' : 'CRED' }`;
