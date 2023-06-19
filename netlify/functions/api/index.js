a802f8abae11c2911d75d846762d9c86fa989db6
Travis Blair <travisjblair@gmail.com>
2023-06-19 16:06:56 -0500

    * update fixes

--- netlify/functions/api/index.js
+++ netlify/functions/api/index.js
@@ -171,62 +171,6 @@ export async function updateStoreCredit(amount, encryptedData, customerId, shop,
 };
 
 
-export async function updateCreditCode(encryptedData, customerId, shop, res, next) {
-  try {
-    if(!encryptedData  ){
-      const err = new Error('Require field of "encryptedData", "customerId"');
-      err.status = 400;
-      return next(err);
-    }
-
-    const decryptedData = decrypt(encryptedData);
-
-    if(decryptedData.customer_id !== customerId){
-      const err = new Error('Unauthorized');
-      err.status = 401;
-      return next(err)
-    }
-
-    let response = await shop.get(`/gift_cards/${decryptedData.id}.json`);
-    const { id, balance, disabled_at } = response.data?.gift_card;
-    const isDisabled = !disabled_at;
-    console.log(1)
-    console.log(response.data?.gift_card)
-    if(isDisabled || parseFloat(balance) <= 0 ){
-      response = await shop.get(`/customers/${customerId}/metafields.json`)
-      const metaFieldToDelete = response?.data?.metafields.find(f => f.namespace === 'fnd' && f.key === 'encrypted_gift_card' )
-      console.log(2, id)
-      if(metaFieldToDelete){
-        await shop.post(`/gift_cards/${id}/disable.json`);
-        console.log(3)
-        await shop.delete(`/customers/${customerId}/metafields/${metaFieldToDelete.id}.json`);
-        console.log(4)
-      }
-    }
-
-    return res.status(200).json({
-      message: `Success`,
-      code: decryptedData.code,
-      balance: parseFloat(balance),
-      isDisabled,
-    });
-  }
-  catch(e){
-    console.log(e.message)
-    try {
-      e = e.toJSON();
-      const err = new Error(`Shopify Err: ${e.message}`)
-      err.status = e.status;
-      return next(err)
-    }
-    catch(e){
-      const err = new Error("Unknown error")
-      err.status = 500;
-      return next(err);
-    }
-  }
-};
-
 export async function deleteMetafieldAndDisableGiftCard(encryptedData, customerId, shop, res, next) {
   try {
     if(!encryptedData || !customerId  ){
--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -1,5 +1,5 @@
 import { bindShopifyService, verifyRequest, verifyWebhookShopify } from '../middleware';
-import {getCustomerMetafields, issueStoreCredit, updateStoreCredit, updateCreditCode, deleteMetafieldAndDisableGiftCard} from '../api'
+import {getCustomerMetafields, issueStoreCredit, updateStoreCredit, deleteMetafieldAndDisableGiftCard} from '../api'
 
 const express = require('express');
 const router = express.Router();
@@ -30,14 +30,64 @@ router.post('/webhook/customer/update', bindShopifyService, async (req, res, nex
     } else if (storeCreditAmount !== 0) {
       await issueStoreCredit(storeCreditAmount, formattedCustomerId, shop, res, next);
     }
-    if (encryptedData) {
-      await updateCreditCode(encryptedData, formattedCustomerId, shop, res, next);
-    }
     res.status(204);
   } catch (error) {
     console.log('Error retrieving customer metafields:', error);
   }
 });
 
+router.post('/store_credit/code', verifyRequest, bindShopifyService, async (req, res, next) => {
+  try {
+    const { encryptedData, customerId } = req.body;
+    if(!encryptedData  ){
+      const err = new Error('Require field of "encryptedData", "customerId"');
+      err.status = 400;
+      return next(err);
+    }
+
+    const decryptedData = decrypt(encryptedData);
+
+    if(decryptedData.customer_id !== customerId){
+      const err = new Error('Unauthorized');
+      err.status = 401;
+      return next(err)
+    }
+
+    let response = await res.locals.shopify.get(`/gift_cards/${decryptedData.id}.json`);
+    const { id, balance, disabled_at } = response.data?.gift_card;
+
+    const isDisabled = !!disabled_at;
+
+    if(isDisabled || parseFloat(balance) <= 0 ){
+      response = await res.locals.shopify.get(`/customers/${customerId}/metafields.json`)
+      const metaFieldToDelete = response?.data?.metafields
+          .find(f => f.namespace === 'fnd' && f.key === 'encrypted_gift_card' )
+      if(metaFieldToDelete){
+        await res.locals.shopify.post(`/gift_cards/${id}/disable.json`);
+        await res.locals.shopify.delete(`/customers/${customerId}/metafields/${metaFieldToDelete.id}.json`);
+      }
+    }
+
+    res.status(200).json({
+      message: `Success`,
+      code: decryptedData.code,
+      balance: parseFloat(balance),
+      isDisabled,
+    });
+  }
+  catch(e){
+    try {
+      e = e.toJSON();
+      const err = new Error(`Shopify Err: ${e.message}`)
+      err.status = e.status;
+      return next(err)
+    }
+    catch(e){
+      const err = new Error("Unknown error")
+      err.status = 500;
+      return next(err);
+    }
+  }
+});
 
 export default router;
